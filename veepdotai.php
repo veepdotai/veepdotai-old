88183367b375c7159bf61416d68bbbaa5ed15c1e
Jean-Christophe Kermagoret <jc@kermagoret.net>
2023-05-09 23:58:39 +0200

    Minor change

--- veepdotai.php
+++ veepdotai.php
@@ -17,7 +17,7 @@
  * Plugin URI:        https://www.veep.ai
  * Description:       Veepdotai is a project to create a complete autonomous virtual presence through voice, from web site to LinkedIn page and, at the end, each social network. A true #nosouci solution :-) 
  * Version:           1.0.0
- * Requires at least: 8.0
+ * Requires at least: 6.0
  * Requires PHP:      8.0
  * Author:            Jean-Christophe Kermagoret
  * Author URI:        https://www.veep.ai
@@ -26,6 +26,21 @@
  * Text Domain:       veepdotai
  * Domain Path:       /languages
  */
+
+ /*
+veepdotai is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+veepdotai is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with veepdotai. If not, see {URI to Plugin License}.
+*/
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
