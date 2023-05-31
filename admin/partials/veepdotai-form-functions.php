b37a9db23ef59c39aab777f7971daad0f37f54b7
Jean-Christophe Kermagoret <jc@kermagoret.net>
2023-05-31 16:22:04 +0200

    Add audio support

--- admin/partials/veepdotai-form-functions.php
+++ admin/partials/veepdotai-form-functions.php
@@ -67,6 +67,14 @@ function display_escaped(bool $enable_voice, string $legend, string $field_name,
                 . $field_element_escaped
                 . '</fieldset>';
 
+    $section_escaped = '<div id="controls">'
+                        . '<button id="recordButton">Record</button>'
+                        . '<button id="pauseButton" disabled>Pause</button>'
+                        . '<button id="stopButton" disabled>Stop</button>'
+                        . '</div>'
+                        . '<p><strong>Recordings:</strong></p>'
+                        . '<ol id="recordingsList"></ol>';
+
     return $section_escaped;
 }
 
